e20b1245707b61947b10783ead143f63cda77a99
V1T1NH000 <empresarialvitorbr@outlook.com>
2025-09-02 18:38:51 -0700

    concertando bug parte4

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 window.__env = {
   NEXT_PUBLIC_SUPABASE_URL: "https://astbpspadyuybblvxpkf.supabase.co",
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzdGJwc3BhZHl1eWJibHZ4cGtmIiwicm9sZSI6ImFubG4ud..."
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzdGJwc3BhZHl1eWJibHZ4cGtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0ODE4MjIsImV4cCI6MjA3MjA1NzgyMn0.udxKFuTgbJGwb-yxwPFRKpxoQoVcxnqLBj2AUNGGLlc"
 };
